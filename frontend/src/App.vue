98018181b0a8501a70a7e1c4b49218d32b3f03a2
AlanGWi <115105025+AlanGWi@users.noreply.github.com>
2022-10-15 16:09:55 -0300

    inicio actualizado

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,48 @@
 <template>
-  <img src="./assets/logo.png" alt="" width="350" height="100" />
+  <img src="./assets/logo.png" alt="" width="470" height="150" />
   
   <div>
-   
-    <p><label> cantidad de espacios {{autos}}</label></p>
+   <h4><label> cantidad de espacios {{autos}}</label></h4>
 
   </div>
 
-       
-  <form action="procesar" method="post" >
+  <div class="container" >
+  <div class="abs-center">
+    <form action="#" class="border p-5 form"  target="_blank" method="get">
+      <div class="form-group ">
+        Ingrese rut:
+        <input type="text" name="rut" id="rut"  
+        required pattern="[0-9]{8}[-]{1}[0-9-k]{1}"
+         placeholder="ej:20846553-8 " class="form-control">
+      </div>
 
-    ingrese rut:
-    <input type="text" id="rut"  required pattern="[0-9]{8}[-]{1}[0-9-k]{1}" placeholder="ej:20846553-8">
-      <br>  
-   
- 
+      <div class="form-group">
+        Nombre:
+         <input type="text" name="nombre" id="nombre"
+        required nombre="a" placeholder="Ejemplo:Jhon" class="form-control">
+      </div>
 
-<p>Nombre: <input type="text" name="nombre" required nombre="a" placeholder="Ejemplo:Jhon"></p>
+      <div class="form-group">
+        Ingrese su apellido
+        <input type="text" name="apellido" id="apellido"
+        required apellido="a" placeholder="Ejemplo:kaios" class="form-control">
+      </div>
 
-<p>Apellido: <input type="text" name="apellido" required apellido="a" placeholder="Ejemplo:kaios"></p>
+      <div class="form-group">
+        Ingrese su patente:
+          <input type="text" name="patente" id="patente"
+          required pattern="[A-Za-z-Az]{4}[0-9]{2}" placeholder="BBBB10" class="form-control">
+      </div>
 
-<p>
+      <div>
+        <button type="submit" value="confirmar" class="btn btn-primary">confirmar </button>
+      </div>
+    </form>
+  </div>
+</div>
+<PatenteAuto></PatenteAuto>
 
-  Ingrese su patente:
-      <input type="text" id="patente" required pattern="[A-Za-z-Az]{4}[0-9]{2}" placeholder="BBBB10">
-      <br> 
-</p>
 
-<p>
-
-  <input type="submit" value="confirmar">
-
-</p>
-
-</form>
 
 </template>
 
@@ -41,9 +50,10 @@
 
 
 import {ref} from 'vue'
+import PatenteAuto from './components/PatenteAuto.vue'
+
+
 let autos = ref(10)
-
-
 
 
 </script>
@@ -54,7 +64,19 @@ let autos = ref(10)
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #222c35;
+  margin-top: 70px;
+}
+
+.abs-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: left;
+  min-height: 10vh;
+  
+}
+.form {
+  width: 450px;
 }
 </style>
---
+++ frontend/src/components/PatenteAuto.vue
@@ -0,0 +1,76 @@
+
+<template>
+    <div id="cronometro">
+        <div id="reloj">
+            00 00 00 00
+          </div>
+        <form name="cron" action="#">
+          <input type="button" value="Empezar" name="boton1"   />
+          <input type="button" value="Parar" name="boton2"  /><br/>
+          
+        </form>
+      </div>
+    </template>
+
+<script type="text/javascript">
+let visor;
+var cro=0;
+let actual;
+let elcrono;
+let emp;
+let cr;
+let cs;
+let sg;
+let mn;
+let ho;
+
+window.onload = function() {
+
+    visor=document.getElementById("reloj"); //localizar pantalla del reloj
+    //asociar eventos a botones: al pulsar el botón se activa su función.
+    document.cron.boton1.onclick = activo; 
+    document.cron.boton2.onclick = pausa;
+    
+    }
+    
+    //botón Empezar 
+    function activo (){   
+         if (document.cron.boton1.value=="Empezar") { //botón en "Empezar"
+         emp=new Date() //fecha inicial (en el momento de pulsar)
+         elcrono=setInterval(tiempo,10); //función del temporizador.
+  
+         document.cron.boton2.disabled=false; //activar botón de pausa
+            }
+         }
+      //parar el cronómetro
+      function pausa() { 
+        clearInterval(elcrono); //parar el crono
+   
+        }		
+
+    //función del temporizador			
+    function tiempo() { 
+        
+         actual=new Date(); //fecha a cada instante
+            //tiempo del crono (cro) = fecha instante (actual) - fecha inicial (emp)
+         cro=actual-emp; //milisegundos transcurridos.
+         cr=new Date(); //pasamos el num. de milisegundos a objeto fecha.
+         cr.setTime(cro); 
+            //obtener los distintos formatos de la fecha:
+         cs=cr.getMilliseconds(); //milisegundos 
+         cs=cs/10; //paso a centésimas de segundo.
+         cs=Math.round(cs); //redondear las centésimas
+         sg=cr.getSeconds(); //segundos 
+         mn=cr.getMinutes(); //minutos 
+         ho=cr.getHours()-21; //horas 
+            //poner siempre 2 cifras en los números		 
+         if (cs<10) {cs="0"+cs;} 
+         if (sg<10) {sg="0"+sg;} 
+         if (mn<10) {mn="0"+mn;} 
+            //llevar resultado al visor.		 
+         visor.innerHTML=ho+" "+mn+" "+sg+" "+cs; 
+         } 
+
+</script>
+
+
