c4fa04ae70b3718c18140485645e13563700970f
Ignacio-Ruiz <71133213+Ignacio-Ruiz@users.noreply.github.com>
2022-10-16 23:02:16 -0300

    Creano base de datos

--- Docker/mongoconection.js
+++
@@ -1,4 +0,0 @@
-const { mongoose, Schema, model } = require('mongoose');
-
-const server = '127.0.0.1:27017'; 
-const database = 'datos';
---
+++ Docker/src/index.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const app = axios();
+
+//Configuraciones; configuracions de la aplicacion
+app.set('port', 3000);
+//varibales intermedias: funciones ejecutadas antes que lleguen al servidor 
+
+//Variables globlales: Colocar ciertos datos para que toda nuestra aplicacion tenga acceso 
+
+//Routes
+
+//Static file
+
+//Inicializacion del servidor 
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
