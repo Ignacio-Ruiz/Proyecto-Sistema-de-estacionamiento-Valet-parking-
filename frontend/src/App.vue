5f877e225118fde0ac298c5d38340003d4ca6343
AlanGWi <alan.gimilio@alumnos.ulagos.cl>
2022-11-23 16:27:49 -0300

    actualizacion

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,7 @@
 
 
   <nav class="navbar navbar-expand-lg   navbar navbar-dark bg-dark  ">
-    <a class="navbar-brand" href="/#home">inicio</a>
+    <a class="navbar-brand" href="/#">inicio</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -12,7 +12,10 @@
           <a class="nav-link" href="/#about">Usuarios <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/#">login</a>
+          <a class="nav-link" href="/#login">login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/#cambio">cambios admin</a>
         </li>
      
       </ul>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,14 +5,6 @@ const routes = [
 
   {
     path: '/',
-    name: 'login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
-  },
-  {
-    path: '/home',
     name: 'home',
     component: HomeView
   },
@@ -23,6 +15,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
   },
   {
     path: '/cambio',
